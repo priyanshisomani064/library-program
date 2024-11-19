d926211760e00a8750e1802106eac48d60a438ba
Priyanshi Somani <111416339+priyanshisomani064@users.noreply.github.com>
2024-11-19 20:47:33 +0530

    Create models.py

---
+++ models.py
@@ -0,0 +1,22 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(50), nullable=False)
+    role = db.Column(db.String(10), nullable=False)  # 'admin' or 'user'
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    available = db.Column(db.Boolean, default=True)
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    issue_date = db.Column(db.Date, nullable=False)
+    return_date = db.Column(db.Date, nullable=False)
+    remarks = db.Column(db.String(200), nullable=True)
+    fine_paid = db.Column(db.Boolean, default=False)
